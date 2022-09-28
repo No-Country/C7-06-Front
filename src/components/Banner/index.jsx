dd119a25d15b6358902a3f75e2545b8ba6c7e584
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-09-28 11:38:25 -0300

    PNC-34 Banner responsive

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from "./Banner.module.sass"
-import animatch from "../../assets/Animatch.png"
+import animatch from "../../assets/banner.png"
 
 const Banner = () => {
   return (
     <div className={styles.container}>
-      <img src={animatch} alt="dog"/>
+      <img src={animatch} alt="dog" className={styles.img}/>
+      <p>Encuenta al mejor compañero para tu mascota</p>
     </div>
   )
 }

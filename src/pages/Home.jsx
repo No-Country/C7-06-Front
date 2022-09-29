f76c0150324dd82682e8ea9e3f652634cd306d8e
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-09-29 13:17:30 +0200

    PNC-39 Pages folder for routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Intro from "../components/Intro";
+import HomeDogs from "../components/HomeDogs";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Intro />
+      <HomeDogs />
+      <Footer />
+    </>
+  );
+};
+export default Home;

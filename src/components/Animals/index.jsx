9f679994b69515ba89fc60f429f8673dfaf8788c
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-09-29 14:59:46 +0200

    PNC-40 Animal card component

---
+++ src/components/Animals/index.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import Card from "../Card";
+import dogsMock from "../../data/dogsmock.json";
+import catsMock from "../../data/catsmock.json";
+import styles from "./Animals.module.sass";
+console.log(dogsMock);
+
+const Animals = () => {
+  const [dogs, setDogs] = useState([]);
+  const [cats, setCats] = useState([]);
+  useEffect(() => {
+    // https://dog.ceo/api/breeds/image/random
+
+    setDogs(dogsMock);
+    setCats(catsMock);
+  }, []);
+
+  return (
+    <>
+      <section className={styles.animalSection}>
+        <h2 className={styles.animalSection__title}>Perros</h2>
+        <h3 className={styles.more}>Ver más</h3>
+        <div className={styles.animalSection__container}>
+          {dogs.map(dog => (
+            <Card key={dog.id} animal={dog} />
+          ))}
+        </div>
+      </section>
+
+      <section className={styles.animalSection}>
+        <h2 className={styles.animalSection__title}>Gatos</h2>
+        <h3 className={styles.more}>Ver más</h3>
+        <div className={styles.animalSection__container}>
+          {cats.map(cat => (
+            <Card key={cat.id} animal={cat} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+export default Animals;
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,35 @@
+import styles from "./Card.module.sass";
+import PropTypes from "prop-types";
+
+const Card = ({ animal }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card__img}>
+        <img src={`/animals/${animal.picture}`} alt={animal.name} />
+      </div>
+      <div className={styles.card__content}>
+        <h3 className={styles.card__title}>{animal.name}</h3>
+        <div className={styles.card__text}>
+          <ul>
+            <li>
+              <span>Raza:</span> {animal.raza}
+            </li>
+            <li>
+              <span>Sexo:</span> {animal.sexo}
+            </li>
+            <li>
+              <span>Edad:</span> {animal.age}
+            </li>
+          </ul>
+        </div>
+        {/* <button className={styles.card__btn}>Ver más</button> */}
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  animal: PropTypes.object.isRequired
+};
+
+export default Card;

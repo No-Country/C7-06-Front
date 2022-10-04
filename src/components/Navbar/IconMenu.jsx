cacc37d10648df8bef8ad1f66a30fe8b17869211
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-10-04 16:30:01 +0200

    PNC-33 Navbar

--- src/components/Navbar/IconMenu.jsx
+++ src/components/Navbar/IconMenu.jsx
@@ -1,10 +1,19 @@
+import { useContext } from "react";
+import { NavbarContext } from "../../contexts/NavbarContext";
 import styles from "./Navbar.module.sass";
+
 const IconMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useContext(NavbarContext);
+
+  const handleClick = e => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className={styles.barsMenu}>
-      <span className={styles.line1_bars}></span>
-      <span className={styles.line2_bars}></span>
-      <span className={styles.line3_bars}></span>
+    <div className={styles.barsMenu} onClick={handleClick}>
+      <span className={`${isMenuOpen && styles.line1_bars}`}></span>
+      <span className={`${isMenuOpen && styles.line2_bars}`}></span>
+      <span className={`${isMenuOpen && styles.line3_bars}`}></span>
     </div>
   );
 };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,18 @@
 import Logo from "../Logo";
 import NavbarMenu from "../NavbarMenu";
 import IconMenu from "./IconMenu";
+import NavbarProvider from "../../contexts/NavbarContext";
 import styles from "./Navbar.module.sass";
 
 const Navbar = () => {
   return (
-    <header className={styles.navbar}>
-      <Logo />
-      <NavbarMenu />
-      <IconMenu />
-    </header>
+    <NavbarProvider>
+      <header className={styles.navbar}>
+        <Logo />
+        <NavbarMenu />
+        <IconMenu />
+      </header>
+    </NavbarProvider>
   );
 };
 

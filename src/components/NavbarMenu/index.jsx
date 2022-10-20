2405164e5f324f4830f645fdf0eb00f38531613d
Alberto <agomezjuan@hotmail.com>
2022-10-20 13:32:53 +0200

    PNC-40 feat: improve navbar menu

--- src/components/NavbarMenu/index.jsx
+++ src/components/NavbarMenu/index.jsx
@@ -6,6 +6,36 @@ import { NavbarContext } from "../../contexts/NavbarContext";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./NavbarMenu.module.sass";
 import { getUserLogged } from "../../Redux/slices/user/userAction";
+import PropTypes from "prop-types";
+
+// Submenú del usuario
+const ProfileSubmenu = ({ handleLogout, handleLinkClick }) => {
+  return (
+    <div className={styles.userSubmenu}>
+      <ul className={styles.userSubmenu_container} onClick={handleLinkClick}>
+        <li>
+          <NavLink to="/user">Mi perfil</NavLink>
+        </li>
+        <li>
+          <Link to="/account">Información personal</Link>
+        </li>
+        <li>
+          <Link to="/pets">Gestión de mascotas</Link>
+        </li>
+        <li>
+          <Link to="/" className={styles.logout} onClick={handleLogout}>
+            Cerrar sesión
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+ProfileSubmenu.propTypes = {
+  handleLogout: PropTypes.func.isRequired,
+  handleLinkClick: PropTypes.func.isRequired
+};
 
 const NavbarMenu = () => {
   const { userLogged } = useSelector(state => state.auth);
@@ -17,13 +47,13 @@ const NavbarMenu = () => {
 
   // Función para cerrar el menú
   const handleLinkClick = e => {
-    setIsMenuOpen(false);
+    setIsMenuOpen(!isMenuOpen);
   };
 
   // Renderizado condicional del menú
   const renderMenu = {
     display: isMenuOpen && "flex",
-    width: isMenuOpen && "350px"
+    width: isMenuOpen & (window.innerWidth < 850) ? "350px" : "100%"
   };
 
   useEffect(() => {
@@ -73,7 +103,7 @@ const NavbarMenu = () => {
       </ul>
       {userLogged ? (
         <>
-          <Link to="/user" className={styles.user} onClick={handleLinkClick}>
+          <Link className={styles.user} onClick={handleLinkClick}>
             <span>
               {loading ? (
                 <>User</>
@@ -86,21 +116,7 @@ const NavbarMenu = () => {
             <FontAwesomeIcon icon={faCircleUser} />
           </Link>
           {isMenuOpen ? (
-            <div className={styles.userSubmenu}>
-              <ul className={styles.userSubmenu_container}>
-                <li>
-                  <Link to="/account">Información personal</Link>
-                </li>
-                <li>
-                  <Link to="/pets">Gestión de mascotas</Link>
-                </li>
-                <li>
-                  <Link to="/" className={styles.logout} onClick={handleLogout}>
-                    Cerrar sesión
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            <ProfileSubmenu handleLogout={handleLogout} handleLinkClick={handleLinkClick} />
           ) : null}
         </>
       ) : null}

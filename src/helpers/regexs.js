f3652710156384cf979b5381fa858ead736b20eb
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-10-20 19:35:24 +0200

    fix: age regex accept 3 digits

--- src/helpers/regexs.js
+++ src/helpers/regexs.js
@@ -15,7 +15,7 @@ export const regexConditions = type => {
     case "phone":
       return /^(\+\d{1,3}( )?)?((\(\d{1,3}\))|\d{1,3})[- .]?\d{3,4}[- .]?\d{4}$/;
     case "age":
-      return /[0-9]{2}/;
+      return /[0-9]{1,3}/;
     default:
       return true;
   }

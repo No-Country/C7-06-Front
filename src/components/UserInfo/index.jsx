e905642ff122bfcc6766dc241c79f504ce2475a0
MartinKun <martinkunbrc@hotmail.com>
2022-10-26 19:53:54 -0300

    load pets and favourites in User profile

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -10,13 +10,13 @@ import Card from "../Card";
 import styles from "./UserInfo.module.sass";
 // import user from "../../data/usermock.json";
 import PropTypes from "prop-types";
-import dogsMock from "../../data/dogsmock.json";
-import catsMock from "../../data/catsmock.json";
 import { Link } from "react-router-dom";
 import userDefault from "../../assets/userDefault.png";
 import { useSelector } from "react-redux";
-
-const allPets = [...dogsMock, ...catsMock];
+import { useEffect, useState } from "react";
+import { apiPub } from "../../helpers/axios";
+import Paginator from "../Paginator";
+import Spinner from "../Spinner";
 
 // Componente: Foto de usuario
 const UserPicture = ({ image, name }) => {
@@ -30,40 +30,142 @@ const UserPicture = ({ image, name }) => {
 // Componente: Mascotas de usuario
 const Pets = props => {
   return (
-    <div className={styles.petsContainer}>
+    <>
       <h3>Mis mascotas</h3>
       <div className={styles.petsContainer_mine}>
-        {allPets
-          .filter(pet => props.pets?.includes(pet.id))
-          .map(pet => (
-            <Card key={pet.id} animal={pet} />
-          ))}
+        {props.pets.map(pet => (
+          <Card key={pet.id} animal={pet} />
+        ))}
         <AddPet />
       </div>
-    </div>
+    </>
   );
 };
 
 // Componente: Favoritos de usuario
 const Favorites = props => {
   return (
-    <div className={styles.petsContainer}>
+    <>
       <h3>Mis favoritos</h3>
       <div className={styles.petsContainer_favorites}>
-        {allPets
-          .filter(pet => props.pets?.includes(pet.id))
-          .map(pet => (
-            <Card key={pet.id} animal={pet} />
-          ))}
+        {props.pets.map(pet => (
+          <Card key={pet.id} animal={pet} />
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
 // Componente: Información de usuario
 const UserInfo = () => {
   const { userInfo } = useSelector(state => state.user);
-  console.log("user", userInfo);
+
+  const [petsParams, setPetsParams] = useState({ pageNumber: 0 });
+  const [favouritesParams, setFavouritesParams] = useState({ pageNumber: 0 });
+
+  const [callbacks, setCallbacks] = useState({
+    firstCallError: false,
+    secondCallError: false,
+    firstCallData: {
+      lastPage: false,
+      pageNumber: 0,
+      totalElements: 0,
+      totalPages: 0,
+      content: []
+    },
+    secondCallData: {
+      lastPage: false,
+      pageNumber: 0,
+      totalElements: 0,
+      totalPages: 0,
+      content: []
+    },
+    firstCallLoading: false,
+    secondCallLoading: false,
+    firstCallResultsEmpty: false,
+    secondCallResultsEmpty: false
+  });
+
+  useEffect(() => {
+    if (userInfo?.id) {
+      loadPets();
+    } else {
+      setCallbacks(prevCallbacks => ({ ...prevCallbacks, firstCallLoading: true }));
+    }
+  }, [petsParams.pageNumber, userInfo.id]);
+
+  useEffect(() => {
+    if (userInfo?.id) {
+      loadFavourites();
+    } else {
+      setCallbacks(prevCallbacks => ({ ...prevCallbacks, secondCallLoading: true }));
+    }
+  }, [favouritesParams.pageNumber, userInfo.id]);
+
+  const loadPetsFromPage = page => {
+    setPetsParams(prevParams => ({ ...prevParams, pageNumber: page }));
+  };
+
+  const loadFavouritesFromPage = page => {
+    setFavouritesParams(prevParams => ({ ...prevParams, pageNumber: page }));
+  };
+
+  const loadPets = () => {
+    setCallbacks(prevCallbacks => ({ ...prevCallbacks, firstCallLoading: true }));
+    apiPub
+      .get(`/users/${userInfo.id}/pets?pageNumber=${petsParams.pageNumber}`)
+      .then(response => {
+        setCallbacks(prevCallbacks => ({
+          ...prevCallbacks,
+          firstCallData: {
+            ...prevCallbacks.firstCallData,
+            lastPage: response.data.lastPage,
+            pageNumber: response.data.pageNumber,
+            totalElements: response.data.totalElements,
+            totalPages: response.data.totalPages,
+            content: response.data.content
+          },
+          firstCallLoading: false
+        }));
+      })
+      .catch(error => {
+        console.log(error);
+        setCallbacks(prevCallbacks => ({
+          ...prevCallbacks,
+          firstCallLoading: false,
+          firstCallError: true
+        }));
+      });
+  };
+
+  const loadFavourites = () => {
+    setCallbacks(prevCallbacks => ({ ...prevCallbacks, firstCallLoading: true }));
+    apiPub
+      .get(`/users/${userInfo?.id}/favourites?pageNumber=${favouritesParams.pageNumber}`)
+      .then(response => {
+        setCallbacks(prevCallbacks => ({
+          ...prevCallbacks,
+          secondCallData: {
+            ...prevCallbacks.firstCallData,
+            lastPage: response.data.lastPage,
+            pageNumber: response.data.pageNumber,
+            totalElements: response.data.totalElements,
+            totalPages: response.data.totalPages,
+            content: response.data.content
+          },
+          secondCallLoading: false
+        }));
+      })
+      .catch(error => {
+        console.log(error);
+        setCallbacks(prevCallbacks => ({
+          ...prevCallbacks,
+          secondCallLoading: false,
+          secondCallError: true
+        }));
+      });
+  };
+
   const loremIpsum =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem.";
   return (
@@ -98,8 +200,42 @@ const UserInfo = () => {
           </div>
         </div>
       </div>
-      <Pets pets={userInfo?.pets} />
-      <Favorites pets={userInfo?.favourite} />
+      <div className={styles.petsContainer}>
+        <Paginator
+          pageNumber={callbacks.firstCallData.pageNumber}
+          totalPages={callbacks.firstCallData.totalPages}
+          loadDataFromPage={loadPetsFromPage}>
+          {callbacks.firstCallLoading ? (
+            <Spinner />
+          ) : callbacks.firstCallError ? (
+            <h3>Se ha producido un error.</h3>
+          ) : (
+            callbacks.firstCallData.content &&
+            (callbacks.firstCallData.content.length === 0 ? (
+              <></>
+            ) : (
+              <Pets pets={callbacks.firstCallData.content} />
+            ))
+          )}
+        </Paginator>
+        <Paginator
+          pageNumber={callbacks.secondCallData.pageNumber}
+          totalPages={callbacks.secondCallData.totalPages}
+          loadDataFromPage={loadFavouritesFromPage}>
+          {callbacks.secondCallLoading ? (
+            <Spinner />
+          ) : callbacks.secondCallError ? (
+            <h3>Se ha producido un error.</h3>
+          ) : (
+            callbacks.secondCallData.content &&
+            (callbacks.secondCallData.content.length === 0 ? (
+              <h3>No se han agregado favoritos.</h3>
+            ) : (
+              <Favorites pets={callbacks.secondCallData.content} />
+            ))
+          )}
+        </Paginator>
+      </div>
     </>
   );
 };
